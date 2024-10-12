7c279d64f42060309812a30dea65be322e97c70e
Fortland2018(Wiktor_Zabka) <adamkowalskipl68@gmail.com>
2024-10-12 13:41:31 +0200

    z motywem

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:algolia/algolia.dart';
+import 'themes/theme_provider.dart';
+import 'themes/dark_mode.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,29 +18,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Hackathon Plock 2024',
+            theme: themeProvider.themeData,
+            darkTheme: darkMode, // Zakładając, że masz zdefiniowany darkMode
+            themeMode: themeProvider.themeMode,
+            home: MyHomePage(title: 'cos'),
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,9 +70,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Algolia Autocomplete"),
+        title: const Text('Nazwa apki'),
+        scrolledUnderElevation: 4,
+        shadowColor: Theme.of(context).colorScheme.shadow,
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: Icon(
+                Provider.of<ThemeProvider>(context).themeData == darkMode
+                    ? Icons.nights_stay
+                    : Icons.wb_sunny,
+                size: 30,
+                color: Provider.of<ThemeProvider>(context).themeData == darkMode
+                    ? Colors.white
+                    : Colors.black,
+              ),
+              onPressed: () {
+                Provider.of<ThemeProvider>(context, listen: false)
+                    .toggleTheme();
+              },
+            ),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
