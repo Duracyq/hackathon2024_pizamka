71f0f71672e5a134cadc636d8fe5610ead6702fa
Fortland2018(Wiktor_Zabka) <adamkowalskipl68@gmail.com>
2024-10-12 22:52:17 +0200

    dziala, dziala wysylanie zdj i odbieranie tekst ale nie ma komunikacji z serwerem open AI, trzeba zrobic jeszcze app bary do pelni szczescia

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:getwidget/getwidget.dart';
 import 'drawer.dart';
+import 'homehome_page.dart';
+import 'nowy_search.dart';
 import 'themes/dark_mode.dart';
 import 'themes/theme_provider.dart';
 
@@ -19,6 +21,13 @@ class _HomePageState extends State<HomePage> {
   String _currentTitle = "Home";
   Color _currentColor = Colors.blue;
   final Color _inactiveColor = Colors.grey;
+  final List<Widget> _pages = [SearchPageNew(), HomeHomePage(), HomeHomePage()];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
 
   final List<TabData> tabs = [
     TabData(
@@ -82,69 +91,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-            /*
-            SizedBox(
-                width: screenWidth,
-                height: screenHeight - 140, // Consider adjusting this value
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: items.length,
-                  scrollDirection: Axis.vertical,
-                  itemBuilder: (context, index) {
-                    double itemHeight = index == 0 ? 120 : 300;
-
-                    if (index == 0) {
-                      // First element, the carousel
-                      return Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: SingleChildScrollView(
-                          child: GFCarousel(
-                            items: _buildCarouselItems(screenWidth, color),
-                            pauseAutoPlayOnTouch: const Duration(seconds: 1),
-                            height: itemHeight,
-                            aspectRatio: 16 / 9,
-                            viewportFraction: 0.9,
-                            autoPlay: true,
-                            autoPlayInterval: const Duration(seconds: 8),
-                            autoPlayAnimationDuration: const Duration(seconds: 1),
-                            autoPlayCurve: Curves.fastOutSlowIn,
-                            enlargeMainPage: true,
-                            hasPagination: true,
-                            initialPage: 0,
-                            enableInfiniteScroll: true,
-                            scrollDirection: Axis.horizontal,
-                          ),
-                        ),
-                      );
-                    } else {
-                      // Other elements, the post tiles
-                      return SizedBox(
-                        height: screenHeight - 255,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          //child: _buildPostTile(context),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ),*/
-          ],
-        ),
-      ),
+      body: _pages[_currentPage],
       bottomNavigationBar: DotCurvedBottomNav(
         hideOnScroll: false,
         indicatorColor: Colors.blue,
@@ -175,6 +122,22 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+class TabData {
+  final Key key;
+  final IconData iconData;
+  final String title;
+  final Color tabColor;
+  final LinearGradient tabGradient;
+
+  TabData({
+    required this.key,
+    required this.iconData,
+    required this.title,
+    required this.tabColor,
+    required this.tabGradient,
+  });
 }
 
 
