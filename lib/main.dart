dd6b948ed15a29efbf17909984025b115f4aa0eb
Duracyq <szymon.kojek@wp.pl>
2024-10-13 00:30:33 +0200

    fix2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'home.dart';
 
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
 
     options: DefaultFirebaseOptions.currentPlatform,
